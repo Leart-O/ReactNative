e0c8fc29b47c85926f39119f39dae84c74775297
Leart Obertinca <leartobertinca6@gmail.com>
2026-04-21 16:59:25 +0200

    ...

--- Flappy_Bird/src/components/bird.js
+++ Flappy_Bird/src/components/bird.js
@@ -1,20 +1,29 @@
 import React from "react";
-import {view} from "react-native";
+import {View, Image, StyleSheet} from "react-native";
 
-const Bird = () => {
-
-    const birdWidth = 50;
-    const birdHeight = 60;
-
+const Bird = ({birdBottom, birdLeft}) => {
+    const birdWidth = 40;
+    const birdHeight = 40;
     return (
-        <View style={{
-            position: "absolute",
-            backgroundColor: "blue",
-            width: birdWidth,
-            height: birdHeight,
-            left: birdLeft - (birdWidth / 2),
-            bottom: birdBottom - (birdHeight / 2)
-            }}>
-        </View>
-    )
+        <Image
+            style={{
+                position: "absolute",
+                left: birdLeft - (birdWidth / 2),
+                bottom: birdBottom - (birdHeight / 2),
+                width: 40,
+                height: 40,
+            }}
+            source={require("../assets/bird1.png")}
+            resizeMode="stretch"
+        />
+    );
 }
+
+const styles = StyleSheet.create({
+    tinyLogo: {
+        width: 40,
+        height: 40,
+    },
+});
+
+export default Bird;
